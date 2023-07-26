223e35a4ab0a41f8522b1558be41b80d6cdca017
Adeosun Oluwaseyi Esther <deosundeola@gmail.com>
2023-07-25 18:44:50 -0700

    changes

--- 101-cocktail_sort_list.c
+++ 101-cocktail_sort_list.c
@@ -9,70 +9,70 @@
 void cocktail_sort_list(listint_t **list)
 {
 	int swapped;
-	listint_t *left = *list;
-	listint_t *right = NULL;
+	listint_t *current, *left = NULL, *right = NULL;
 
-	if (list == NULL || *list == NULL || (*list)->next == NULL)
+	if (*list == NULL || (*list)->next == NULL)
 		return;
 	do {
 		swapped = 0;
-		left = *list;
+		current = left = *list;
 
-		while (left->next != right)
+		while (current->next != right)
 		{
-			if (left->n > left->next->n)
+			if (current->n > current->next->n)
 			{
-				swap_nodes(list, left, left->next);
+				if (current->prev != NULL)
+					current->prev->next = current->next;
+				else
+					*list = current->next;
+				
+				current->next->prev = current->prev;
+				current->prev = current->next;
+				current->next = current->next->next;
+
+				if (current->next != NULL)
+					current->next->prev = current;
+
+				current->prev->next = current;
+
 				swapped = 1;
+				print_list(*list);
 			}
 			else
 			{
-				left  = left->next;
+				current = current->next;
 			}
 		}
+		right = current;
 		if (!swapped)
 			break;
+		swapped = 0;
 
-		right = left;
-		while (left->prev != NULL)
+		while (current->prev != left)
 		{
-			if (left->n < left->prev->n)
+			if (current->n < current->prev->n)
 			{
-				swap_nodes(list, left->prev, left);
+				if(current->next != NULL)
+					current->next->prev = current->prev;
+				else
+					right = current->prev;
+
+				current->prev->next = current->next;
+				current->next = current->prev;
+				current->prev = current->prev->prev;
+
+				if (current->prev != NULL)
+					current->prev->next = current;
+
+				current->next->prev = current;
 				swapped = 1;
+				print_list(*list);
 			}
 			else
 			{
-				left = left->prev;
+				current = current->prev;
 			}
 		}
+		left = current;
 	} while (swapped);
-}
-
-/**
-* swap_nodes - Swaps two nodes in a doubly linked list.
-* @list: Pointer to the pointer of the head of the list.
-* @node1: Pointer to the first node to be swapped
-* @node2: Pointer to the second node to be swapped
-*
-* Return: no return(void).
-*/
-void swap_nodes(listint_t **list, listint_t *node1, listint_t *node2)
-{
-	if (list == NULL || *list == NULL || node1 == NULL || node2 == NULL)
-		return;
-
-	if (node1->prev != NULL)
-		node1->prev->next = node2;
-	else
-		*list = node2;
-
-	if (node2->next != NULL)
-		node2->next->prev = node1;
-
-	node1->next = node2->next;
-	node2->prev = node1->prev;
-	node1->prev = node2;
-	node2->next = node1;
-	print_list(*list);
 }
